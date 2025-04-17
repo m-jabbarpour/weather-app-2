9df21d778cbb0aa7e340b5093c16997f021c3051
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2025-04-17 22:48:57 +0330

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+// Env Variables
+export const appId = import.meta.env.VITE_WEATHER_APP_ID;
+
+export const weatherIconsMapping = {
+  "01d": "day",
+  "01n": "night",
+  "02d": "cloudy-day-3",
+  "02n": "cloudy-night-3",
+  "03d": "cloudy",
+  "03n": "cloudy",
+  "04d": "cloudy",
+  "04n": "cloudy",
+  "09d": "rainy-6",
+  "09n": "rainy-6",
+  "10d": "rainy-3",
+  "10n": "rainy-6",
+  "11d": "thunder",
+  "11n": "thunder",
+  "13d": "snowy-3",
+  "13n": "snowy-6",
+  "50d": "mist",
+  "50n": "mist",
+};
