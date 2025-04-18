55c36dfc69823d8a3000a8374bde7335a209ea56
Mortaza Jabbarpour <mortaza.jabbarpour@gmail.com>
2025-04-18 17:59:39 +0330

    Add PWA Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,37 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Weather",
+        short_name: "Weather",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#052f4a",
+        background_color: "#052f4a",
+        description:
+          "An app to see weather info based on your location or entered city",
+        icons: [
+          {
+            src: "/app-icons/icon-48.svg",
+            sizes: "48x48",
+            type: "image/svg",
+          },
+          {
+            src: "/app-icons/icon-192.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
 });
